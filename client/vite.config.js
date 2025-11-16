ac30ebd2e7376448a29942a2d6204118ce22f94f
saivamshi4121 <palamurivamshi2005@gmail.com>
2025-11-16 13:37:38 +0530

    resolved the vite base

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  base: "/", // REQUIRED FOR RENDER - Ensures JS bundle loads correctly
   publicDir: "public",
   build: {
     outDir: "dist",
